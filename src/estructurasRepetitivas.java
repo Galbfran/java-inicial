2b5f02d4f0f4c4d90b1424716cf0be86d7affe0e
Galbfran <francogalbiati984@gmail.com>
2023-10-04 17:48:50 -0300

    subo bucles while y for

---
+++ src/estructurasRepetitivas.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+
+public class estructurasRepetitivas {
+    public static void main(String[] args) {
+        // bucle while
+        int contador = 0;
+
+        while (contador < 10){
+            System.out.println("contador es: " + contador);
+            contador++;
+        }
+
+        for (int i = 0 ; i<10 ;i++){
+            System.out.println("el valor de i es: " + i);
+        }
+    }
+}
