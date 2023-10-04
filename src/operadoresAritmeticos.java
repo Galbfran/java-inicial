1258b31e20d6ad84340bb4561263fb8dc88eb2cf
Galbfran <francogalbiati984@gmail.com>
2023-10-04 12:08:34 -0300

    subo variables

---
+++ src/operadoresAritmeticos.java
@@ -0,0 +1,6 @@
+public class operadoresAritmeticos {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,26 @@
+public class variables {
+    public static void main(String[] args){
+        // entero  => int
+        int numeroEntero = 10;
+        //decimales => double
+        double numeroDecimal = 5.5;
+        //boleanos => Boolean
+        Boolean verdadero = true;
+        Boolean falso = false;
+        //caracteres => char
+        char caracter = 'f';
+        // cadena texto => String
+        String texto = "hola mundo";
+        //entero largo => long
+        long numeroLargo = 1L;
+
+        System.out.println(numeroEntero);
+        System.out.println(numeroDecimal);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(caracter);
+        System.out.println(texto);
+        System.out.println(numeroLargo);
+    }
+
+}
